08b0ab8063c109335d0e5899717d29df193fb138
dls0122 <49166986+dls0122@users.noreply.github.com>
2022-05-01 20:28:35 -0500

    Update main.go

--- main.go
+++ main.go
@@ -194,11 +194,7 @@ func main() {
 		panic(err)
 	}
 	
-	port := os.Getenv("PORT")
-	if port == "" {
-        port = "8080"
-	}
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	
 
 	// Test the database connection
 	//err = db.Ping()
@@ -221,6 +217,11 @@ func main() {
 		// Pull the data once a day
 		// You might need to pull Taxi Trips and COVID data on daily basis
 		// but not the unemployment dataset becasue its dataset doesn't change every day
+		port := os.Getenv("PORT")
+		if port == "" {
+        		port = "8080"
+		}
+		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 		time.Sleep(24 * time.Hour)
 	}
 	
